fb419283f3c713aa72251125fc627dc22438c4e9
William Shi <willshi101@gmail.com>
2024-11-16 13:09:24 -0500

    Tried to implement Commons CSV

--- src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
+++ src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
@@ -1,8 +1,13 @@
 package com.example.MentalHealthPredictor;
 
-public class DataSetResponseParser{
+import org.apache.commons.csv.*;
+import java.io.*;
 
+public class DataSetResponseParser{
+    
 }
+
+
 
 class SurveyResponse{
 
