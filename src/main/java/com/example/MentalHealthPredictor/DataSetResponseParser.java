845057b07926905c2327a378b1468b3b3a61ae18
Dennis Do <dennisnasa@gmail.comgit config --global user.email>
2024-11-19 15:16:59 -0500

    This prints out servey.csv file

--- src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
+++ src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
@@ -1,7 +1,13 @@
 package com.example.MentalHealthPredictor;
 
 import org.apache.commons.csv.*;
-import java.io.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.charset.*;
+
+
 
 public class DataSetResponseParser{
     
@@ -10,5 +16,31 @@ public class DataSetResponseParser{
 
 
 class SurveyResponse{
+    public SurveyResponse(){
 
+    }
+    public void printSer() throws IOException{
+    String documentsPath = System.getProperty("user.home") + "/Documents";
+    String filename = "survey.csv";
+
+    Path documentsDirectory = Paths.get(documentsPath);
+    Path csvPath = documentsDirectory.resolve(filename);
+
+    @SuppressWarnings("deprecation")
+    CSVParser csvParser = CSVParser.parse(
+        csvPath, 
+        Charset.defaultCharset(),
+        CSVFormat.DEFAULT.withHeader(
+        "Timestamp", "Mental Ilnness", "Age", "Gender", "Country", "state", "self_employed", 
+            "family_history", "treatment", "no_employees", "remote_work", "tech_company",
+            "benefits", "care_options", "wellness_program", "seek_help", "anonymity", "leave",
+            "mental_health_consequence", "phys_health_consequence", "coworkers", "supervisor",
+            "mental_health_interview", "phys_health_interview", "mental_vs_physical","obs_consequence" ,
+            "comments"));
+    csvParser.forEach(csvRecord -> {
+        System.out.println(csvRecord.toMap());
+    }
+    
+    );
+    }
 }
--- src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
+++ src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
@@ -1,13 +1,35 @@
 package com.example.MentalHealthPredictor;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.*;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MentalHealthPredictor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		String documentsPath = System.getProperty("user.home") + "/370 Project/CS370Project/src/main/resources/static" ;
+		//C:\Users\denni\370 project\CS370Project\src\main\resources\static
+    String filename = "survey.csv";
+
+    Path documentsDirectory = Paths.get(documentsPath);
+    Path csvPath = documentsDirectory.resolve(filename);
+
+    CSVParser csvParser = CSVParser.parse(csvPath, Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
+    csvParser.forEach(csvRecord -> {
+        System.out.println(csvRecord.toMap());
+    }
+    
+    );
 		SpringApplication.run(MentalHealthPredictor.class, args);
+		
 	}
 
 }
