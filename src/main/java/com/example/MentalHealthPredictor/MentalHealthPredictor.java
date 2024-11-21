5cfb67f0fe848f1f9ca8642be4ee8af91c402e5b
Anthony He <heanthony2004@gmail.com>
2024-11-21 14:58:11 -0500

    YEs

--- src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
+++ src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
@@ -15,20 +15,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MentalHealthPredictor {
 
 	public static void main(String[] args) throws IOException {
-		String documentsPath = System.getProperty("user.dir")+ "/src/main/resources/static";
+		String documentsPath = System.getProperty("user.dir")  ;
 		//System.out.println(documentsPath);
 		//C:\Users\denni\370 project\CS370Project\src\main\resources\static
     String filename = "survey.csv";
 
-    Path documentsDirectory = Paths.get(documentsPath);
-    Path csvPath = documentsDirectory.resolve(filename);
+    Path documentsDirectory = Paths.get(documentsPath,"src/main/resources/static");
+     Path csvPath = documentsDirectory.resolve(filename);
+	//File f = new File(csvPath.toFile();
 		System.out.println(csvPath.toString());
-    // CSVParser csvParser = CSVParser.parse(csvPath, Charset.defaultCharset(), CSVFormat.DEFAULT);
-    // csvParser.forEach(csvRecord -> {
-    //      System.out.println(csvRecord.toMap());
-    //  }
+	CSVParser csvParser = CSVParser.parse(csvPath,Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
+    csvParser.forEach(csvRecord -> {
+          System.out.println(csvRecord.toMap());
+      }
     
-    //  );
+      );
 		SpringApplication.run(MentalHealthPredictor.class, args);
 		
 	}
