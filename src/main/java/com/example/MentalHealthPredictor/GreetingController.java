06eae89b19df8160b85d4c94a64bdbf0ccd56cce
William Shi <willshi101@gmail.com>
2024-11-23 11:44:57 -0500

    Finally fixed the button display

--- src/main/java/com/example/MentalHealthPredictor/GreetingController.java
+++ src/main/java/com/example/MentalHealthPredictor/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
+	String name = "Dennis";
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
