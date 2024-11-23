9e7d25765fe94d7563a663695276f5083942f9af
Anthony He <heanthony2004@gmail.com>
2024-11-23 11:12:05 -0500

    I have stored all the data into an array named

--- src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
+++ src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
@@ -4,7 +4,10 @@ import org.apache.commons.csv.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.io.IOException;
+
 import java.nio.charset.*;
 
 
@@ -17,6 +20,8 @@ public class DataSetResponseParser{
 
 
 class SurveyResponse{
+    List<String> columnHeaders;
+    String surveyData[][];
     public SurveyResponse(String filename) throws IOException{
         String documentsPath = System.getProperty("user.dir")  ;
         
@@ -24,11 +29,19 @@ class SurveyResponse{
         Path documentsDirectory = Paths.get(documentsPath,"src/main/resources/static");
         Path csvPath = documentsDirectory.resolve(filename);
 
-	    CSVParser csvParser = CSVParser.parse(csvPath,Charset.defaultCharset(), CSVFormat.DEFAULT.withHeader());
-        csvParser.forEach(csvRecord -> {
-          System.out.println(csvRecord.toMap());
-      }
-    
-      );
+	    CSVParser csvParser = CSVParser.parse(csvPath,Charset.defaultCharset(),CSVFormat.EXCEL.withHeader());
+        columnHeaders = csvParser.getHeaderNames();
+        
+
+        List<CSVRecord> records = csvParser.getRecords();
+        surveyData = new String[records.size()][records.get(0).size()];
+        for(int i =0;i<records.size();i++){
+            CSVRecord curr = records.get(i);
+            for(int j =0;j<records.get(i).size();j++){
+                surveyData[i][j] = curr.get(j);
+            }
+        }
+        System.out.println("Demo Row 1 Col 1");
+        System.out.println(columnHeaders.get(0) + ": " + surveyData[0][0] );
     }
 }
--- src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
+++ src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
@@ -1,13 +1,7 @@
 package com.example.MentalHealthPredictor;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.charset.*;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
