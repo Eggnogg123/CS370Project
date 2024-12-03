73bf1c65a6e81e6b809ef97aed0b0807d4a690db
Dennis Do <dennisnasa@gmail.comgit config --global user.email>
2024-12-03 18:14:03 -0500

    added construtors and methods in currentsessionresponses

--- src/main/java/com/example/MentalHealthPredictor/CurrentSessionResponses.java
+++ src/main/java/com/example/MentalHealthPredictor/CurrentSessionResponses.java
@@ -1,6 +1,16 @@
 package com.example.MentalHealthPredictor;
 
 
-public class CurrentSessionResponses {
-    
+public class CurrentSessionResponses {      
+    private String[] inputs;
+    private String[] questions;
+
+    public CurrentSessionResponses(int counter){
+        inputs = new String[counter];
+        questions = new String[counter];
+
+    }
+
+    //a method that takes in a input object and it gets the user input and corresponding question from the object
 }
+
--- src/main/java/com/example/MentalHealthPredictor/Input.java
+++ src/main/java/com/example/MentalHealthPredictor/Input.java
@@ -6,4 +6,9 @@ public class Input {
     public Input(String in){
         input = in;
     }
+    //pivate the variables
+    //expand the constructor to take and also store the current question 
+    //make an method that returns the current question and the method that returns the current input
+    
+    
 }
