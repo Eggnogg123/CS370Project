4e2e0dbb9bcb76d47a25a0e6a12ab1e5ca25e195
Anthony He <heanthony2004@gmail.com>
2024-12-03 17:59:40 -0500

    THE questions are in the code

--- src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
+++ src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
@@ -16,8 +16,7 @@ import java.nio.charset.*;
 public class DataSetResponseParser{
     private SurveyResponse data;
     private int numResponses,predictedVarCol = -1;
-    private String parsedData[][];
-    private String parsedColumnQuestion[];
+    private String parsedData[][],parsedColumnName[],parsedColumnQuestions[];
 
 //Constructor
     public DataSetResponseParser(String filename) throws IOException{
@@ -29,37 +28,9 @@ public class DataSetResponseParser{
                 break;
             }
         }
-        //Check for number of usable responses in csv. Responses that have a answer for the predicted variable
-        for(int i = 0;i<data.getNumRows();i++){
-            if(data.getRecord(i, predictedVarCol).equals(""))numResponses--;
-        }
-        //The parsed data array is smaller than the one in SurveyResponse, since 3 columns of data not
-        //used in the algorithm get removed, invalid responses are also skipped over
-        parsedData = new String[numResponses][data.getNumCol() - 3];
-        parsedColumnQuestion = new String[data.getNumCol() - 3];
-        for(int i =0,x = 0;i < parsedColumnQuestion.length;i++,x++){
-            String s = data.getColumnName(x);
-            while(s.equals("Timestamp") || s.equals("treatment")|| s.equals("comments")){
-                x++;
-                s = data.getColumnName(x);
-            }
-            parsedColumnQuestion[i] = data.getColumnName(x);
-        }
-        for(int i =0,x = 0;i<data.getNumRows();i++){
-            if(data.getRecord(i,predictedVarCol).equals(""))continue;
-            for(int j =0,y = 0;j< data.getNumCol();j++){
-                String s = data.getColumnName(j);
-                if(s.equals("Timestamp") || s.equals("treatment")|| s.equals("comments")){
-                    continue;
-                }
-                if(s.equals("Gender")){
-                    parsedData[x][y] = sortGender(data.getRecord(i, j));    
-                }
-                else parsedData[x][y] = data.getRecord(i, j);
-                y++;
-            }
-            x++;
-        }
+        parseResponse();
+        buildQuestions(parsedColumnName.length - 1);
+        for(int i =0;i<parsedColumnQuestions.length;i++)System.out.println(parsedColumnQuestions[i]);
         
     }
 
@@ -71,7 +42,7 @@ public class DataSetResponseParser{
         return parsedData[0].length;
     }
     public String getParsedColName(int in){
-        return parsedColumnQuestion[in];
+        return parsedColumnName[in];
     }
 // Get a subtable of the parsed Dataset, used for bootstrap
     public String[][] getSubTable(int rowNotUsed,int featureSelectedOut,int rows,int cols){
@@ -97,11 +68,126 @@ public class DataSetResponseParser{
             if(j == featureSelectedOut){
                 j++;
             } 
-            table[i] = parsedColumnQuestion[j];
+            table[i] = parsedColumnName[j];
         }
         return table;
     }
-
+//Parse the survey
+    private void parseResponse(){
+        //Check for number of usable responses in csv. Responses that have a answer for the predicted variable
+        for(int i = 0;i<data.getNumRows();i++){
+            if(data.getRecord(i, predictedVarCol).equals(""))numResponses--;
+        }
+        //The parsed data array is smaller than the one in SurveyResponse, since 3 columns of data not
+        //used in the algorithm get removed, invalid responses are also skipped over
+        parsedData = new String[numResponses][data.getNumCol() - 3];
+        parsedColumnName = new String[data.getNumCol() - 3];
+        for(int i =0,x = 0;i < parsedColumnName.length;i++,x++){
+            String s = data.getColumnName(x);
+            while(s.equals("Timestamp") || s.equals("treatment")|| s.equals("comments")){
+                x++;
+                s = data.getColumnName(x);
+            }
+            parsedColumnName[i] = data.getColumnName(x);
+        }
+        for(int i =0,x = 0;i<data.getNumRows();i++){
+            if(data.getRecord(i,predictedVarCol).equals(""))continue;
+            for(int j =0,y = 0;j< data.getNumCol();j++){
+                String s = data.getColumnName(j);
+                if(s.equals("Timestamp") || s.equals("treatment")|| s.equals("comments")){
+                    continue;
+                }
+                if(s.equals("Gender")){
+                    parsedData[x][y] = sortGender(data.getRecord(i, j));    
+                }
+                else parsedData[x][y] = data.getRecord(i, j);
+                y++;
+            }
+            x++;
+        }
+    }
+//CHANGES TO THE QUESTIONS GO HERE
+    private void buildQuestions(int columns){
+        parsedColumnQuestions = new String[columns];
+//IMPORTANT- IN THE EVENT THAT SURVEY COLUMN ORDER IS CHANGED THIS WILL OUTPUT AN ERROR
+        for(int i = 0,j = 1;i< parsedColumnQuestions.length;i++,j++){
+            switch (parsedColumnName[j]) {
+                case "Age":
+                parsedColumnQuestions[i] = "What is your Age?";
+                break;
+                case "Gender":
+                parsedColumnQuestions[i] = "What is your Gender?";
+                break;
+                case "Country":
+                parsedColumnQuestions[i] = "What is your Country of Residence?";
+                break;
+                case "state":
+                parsedColumnQuestions[i] = "If you live in the US, what is your State of residence?";
+                break;
+                case "self_employed":
+                parsedColumnQuestions[i] = "Are you currently self-employed or not?";
+                break;
+                case "family_history":
+                parsedColumnQuestions[i] = "Do you have a family history of mental illness?";
+                break;
+                case "no_employees":
+                parsedColumnQuestions[i] = "How many employees does your company or organization have?";
+                break;
+                case "remote_work":
+                parsedColumnQuestions[i] = "Do you work remotely (outside of an office) at least 50% of the time?";
+                break;
+                case "tech_company":
+                parsedColumnQuestions[i] = "Is your employer primarily a tech company/organization?";
+                break;
+                case "benefits":
+                parsedColumnQuestions[i] = "Does your employer provide mental health benefits?";
+                break;
+                case "care_options":
+                parsedColumnQuestions[i] = "Do you know the options for mental health care your employer provides?";
+                break;
+                case "wellness_program":
+                parsedColumnQuestions[i] = "Has your employer ever discussed mental health as part of an employee wellness program?";
+                break;
+                case "seek_help":
+                parsedColumnQuestions[i] = "Does your employer provide resources to learn more about mental health issues and how to seek help?";
+                break;
+                case "anonymity":
+                parsedColumnQuestions[i] = "Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?";
+                break;
+                case "leave":
+                parsedColumnQuestions[i] = "How easy is it for you to take medical leave for a mental health condition?";
+                break;
+                case "mental_health_consequence":
+                parsedColumnQuestions[i] = "Do you think that discussing a mental health issue with your employer would have negative consequences?";
+                break;
+                case "phys_health_consequence":
+                parsedColumnQuestions[i] = "Do you think that discussing a physical health issue with your employer would have negative consequences?";
+                break;
+                case "coworkers":
+                parsedColumnQuestions[i] = "Would you be willing to discuss a mental health issue with your coworkers?";
+                break;
+                case "supervisor":
+                parsedColumnQuestions[i] = "Would you be willing to discuss a mental health issue with your direct supervisor(s)?";
+                break;
+                case "mental_health_interview":
+                parsedColumnQuestions[i] = "Would you bring up a mental health issue with a potential employer in an interview?";
+                break;
+                case "phys_health_interview":
+                parsedColumnQuestions[i] = "Would you bring up a physical health issue with a potential employer in an interview?";
+                break;
+                case "mental_vs_physical":
+                parsedColumnQuestions[i] = "Do you feel that your employer takes mental health as seriously as physical health?";
+                break;
+                case "obs_consequence":
+                parsedColumnQuestions[i] = "Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?";
+                break;
+                
+                default:
+                System.out.println("ERROR MISSING COLUMN: " + parsedColumnName[j]);
+                System.exit(0);
+            }
+        }
+    }
 //Helps with parsing through the Gender Column of the survey
     private String sortGender(String in){
         switch (in) {
