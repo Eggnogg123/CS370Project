5c9c0532b4a55c82a57ed3c52d6977ac9788b1bd
Anthony He <heanthony2004@gmail.com>
2024-12-05 13:41:46 -0500

    seperate SurveyResponse into its own class and added a getChoices method to DataSetResponseParser that takes in String QuestionName and returns set

--- src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
+++ src/main/java/com/example/MentalHealthPredictor/DataSetResponseParser.java
@@ -1,17 +1,11 @@
 package com.example.MentalHealthPredictor;
 
-import org.apache.commons.csv.*;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.IOException;
-import java.nio.charset.*;
 
 
 
@@ -438,44 +432,3 @@ public class DataSetResponseParser{
 
 
 
-class SurveyResponse{
-    List<String> columnHeaders;
-    String surveyResponses[][], filename;
-    public SurveyResponse(String fname) throws IOException{
-        String documentsPath = System.getProperty("user.dir")  ;
-        filename = fname;
-
-        Path documentsDirectory = Paths.get(documentsPath,"src/main/resources/static");
-        Path csvPath = documentsDirectory.resolve(filename);
-
-	    CSVParser csvParser = CSVParser.parse(csvPath,Charset.defaultCharset(),CSVFormat.EXCEL.withHeader());
-        columnHeaders = csvParser.getHeaderNames();
-        
-
-        List<CSVRecord> records = csvParser.getRecords();
-        surveyResponses = new String[records.size()][records.get(0).size()];
-        for(int i =0;i<records.size();i++){
-            CSVRecord curr = records.get(i);
-            for(int j =0;j<records.get(i).size();j++){
-                surveyResponses[i][j] = curr.get(j);
-            }
-        }
-        
-    }
-
-    String getRecord(int row,int col){
-        if(row >= surveyResponses.length || col >= surveyResponses[0].length
-           || row < 0 || col < 0)return "INVALID RECORD";
-        return surveyResponses[row][col];
-    }
-    String getColumnName(int col){
-        if(col < 0 || col > columnHeaders.size())return "INVALID VARIABLE COLUMN";
-        return columnHeaders.get(col);
-    }
-    int getNumRows(){
-        return surveyResponses.length;
-    }
-    int getNumCol(){
-        return surveyResponses[0].length;
-    }
-}
