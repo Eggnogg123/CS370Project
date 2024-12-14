0b026c40f82bbe063d861643f4e13d37613848c8
William Shi <willshi101@gmail.com>
2024-12-14 10:42:13 -0500

    Dynamic Link

--- src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
+++ src/main/java/com/example/MentalHealthPredictor/MentalHealthPredictor.java
@@ -1,9 +1,6 @@
 package com.example.MentalHealthPredictor;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.io.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
